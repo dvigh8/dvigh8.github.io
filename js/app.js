fff66174f0c4ddbb356f7e8effd219d8be1d1684
David Josephs <josephs.david.b@gmail.com>
2016-05-19 12:23:48 -0400

    starting to use angular js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+angular.module('app',[]);
+
+angular.module('app').controller('MainCtrl', function($scope){
+    
+    $scope.message= 'hello';
+    
+    $scope.updateMessage= function(message){
+        $scope.message = message;
+    };
+
+});
