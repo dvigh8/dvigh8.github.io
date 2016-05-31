fe3d7c4afe715a4839a695011e54aed0d0d53fb9
David Josephs <josephs.david.b@gmail.com>
2016-05-31 14:17:45 -0400

    More added to babylon

--- js/game.js
+++ js/game.js
@@ -28,18 +28,21 @@ function initScene() {
 
     // Create scene
     scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3(.504,.768,.952); 
 
     // Create the camera
-    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0,1.7,0), scene);
+    camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0,1.7,0), scene);
     camera.setTarget(new BABYLON.Vector3(0,0,10));
     camera.attachControl(canvas);
     
     var floor = createFloor(scene);
-    //var grass = new BABYLON.StandardMaterial("grass", scene);
-    //var grasTexture = new BABYLON.GrassProceduralTexture("grass", 512, scene);
+    var grassMaterial = new BABYLON.StandardMaterial("bawl", scene);
+    var grassTexture = new BABYLON.GrassProceduralTexture("textbawl", 256, scene);
+    grassMaterial.ambientTexture = grassTexture;
+    floor.material = grassMaterial;
 
     // Create light
-    var light = new BABYLON.PointLight("light", new BABYLON.Vector3(100,101,0), scene);
+    var light = new BABYLON.PointLight("light", new BABYLON.Vector3(10,10,0), scene);
    
     engine.runRenderLoop(function () {
         scene.render();
@@ -48,7 +51,7 @@ function initScene() {
 function initGame(){
 
     
-    var sphere = BABYLON.Mesh.CreateSphere("sphere", 10, 1, scene);
+    var sphere = BABYLON.Mesh.CreateSphere("sphere", 10, 0.07366, scene);
     sphere.position.y= 1.7;
     
     var animationBox = new BABYLON.Animation("myAnimation", "position.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
@@ -59,24 +62,38 @@ function initGame(){
    //At the animation key 0, the value of scaling is "1"
     keys.push({
         frame: 0,
-        value: 200
+        value: 18.4404
     });
  
     //At the animation key 20, the value of scaling is "0.2"
     keys.push({
         frame: 20,
-        value: -20
+        value: -5
+    });
+    keys.push({
+        frame: 30,
+        value: 1
+    });
+    keys.push({
+        frame: 100,
+        value: 1
     });
  
     //At the animation key 100, the value of scaling is "1"
     keys.push({
-        frame: 100,
-        value: 200
+        frame: 180,
+        value: 18.4404
     });
     animationBox.setKeys(keys);
 
     sphere.animations.push(animationBox);
-    scene.beginAnimation(sphere, 0, 100, true);
+    scene.beginAnimation(sphere, 0, 180, true);
+    var material = new BABYLON.StandardMaterial("material01", scene);
+
+    sphere.material = material;
+
+    material.diffuseTexture = new BABYLON.Texture("baseball.png", scene);
+    
     console.log("animated")
 }        
 function createFloor(scene) {
